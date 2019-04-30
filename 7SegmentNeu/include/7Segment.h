50bd944d037d136f97cb735b2774d97efcb12a0b
Franz <inf17052@lehre.dhbw-stuttgart.de>
2019-04-30 03:38:34 +0200

    got enlightened by u/Sumbuble

--- 7SegmentNeu/include/7Segment.h
+++ 7SegmentNeu/include/7Segment.h
@@ -4,16 +4,40 @@
 #include <array>
 #include <iostream>
 
+template <typename... Args>
 class MultiDigit{
 public:
-    template <typename... Args>
-    constexpr MultiDigit(Args...args) {
-        std::array<char, (sizeof...(args))* 20 + 1> printString{0};
-        printString[0] = 'a';
-        printString[1] = '\0';
+    char printString[sizeof...(Args) * 20 + 1];
+    int digits[sizeof...(Args)];
+
+
+    constexpr MultiDigit(Args...args):
+        printString{},
+        digits{args...}
+    {
+        unsigned int nextWrite = 0;
+        for(unsigned int row = 0; row < 5; ++row ) {
+            for(unsigned int digitNr = 0; digitNr < sizeof...(Args); ++digitNr) {
+                printString[nextWrite] = digitChars[digits[digitNr]][row][0];
+                ++nextWrite;
+                printString[nextWrite] = digitChars[digits[digitNr]][row][1];
+                ++nextWrite;
+                printString[nextWrite] = digitChars[digits[digitNr]][row][2];
+                ++nextWrite;
+                printString[nextWrite] = ' ';
+                ++nextWrite;
+            }
+            printString[nextWrite - 1] = '\n';
+        }
+
+        printString[nextWrite] = '\0';
     };
+
+    operator const char*() const {
+        return this->printString;
+    }
 private:
-    const char digits2[10][5][3]  = {
+    const char digitChars[10][5][3]  = {
         {
             {' ', '-', ' '},
             {'|', ' ', '|'},
--- 7SegmentNeu/src/main.cpp
+++ 7SegmentNeu/src/main.cpp
@@ -1,6 +1,8 @@
 #include "../include/7Segment.h"
+#include <stdio.h>
 
 int main() {
-    constexpr MultiDigit md{1,2};
+    constexpr MultiDigit md{1,2,3,5,6,7,7,6,5,4};
+    printf("mein string: \n%s", static_cast<const char*>(md));
     return 0;
 }
